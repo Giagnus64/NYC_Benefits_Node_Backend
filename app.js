c59be9884f8d27231d60678b981337d3defeecea
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-02-17 17:58:32 -0500

    Finishedaxios testing

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-const express = require('express');
-const app = express();
-const request = require('request');
+//const express = require('express');
+// const app = express();
+const axios = require('axios').default;
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
-});
+const url = 'https://screeningapi.cityofnewyork.us/authToken';
 
-//PORT
-const port = process.env.PORT || 3000
+let authToken;
 
-app.listen(port, () => console.log(`Listening on ${port}`))
+axios({
+    method: 'post',
+    url: url,
+    data: {
+        "username": "gianfranconuschese",
+        "password": "Checkup!234"
+    },
+    headers: {
+    'Content-Type': 'application/json',
+    'cache-control': 'no-cache'
+    }
+})
+.then((response) => {
+    //const parsed = JSON.parse(response.body);
+    if(response.data.type === 'SUCCESS'){
+        authToken = response.data.token
+        //return response.data.token
+    } else{
+        // report failure
+    }
+    
+})
+.catch((err) => {
+    console.log(err)
+})
+
+setTimeout(() => console.log(authToken), 3000)
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hello world");
+// });
+
+// //PORT
+// const port = process.env.PORT || 3000
+
+// app.listen(port, () => console.log(`Listening on ${port}`))
